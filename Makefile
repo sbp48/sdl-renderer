5f85466975499131c2ddcd8859e6bf78b429f1e4
sbp48 <peteris.poikans@proton.me>
2026-07-16 02:42:01 +0300

    loads image now

--- Makefile
+++ Makefile
@@ -1,21 +1,26 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -std=c11 $(shell pkg-config --cflags sdl2)
-LDFLAGS = $(shell pkg-config --libs sdl2)
+CFLAGS = -Wall -Wextra -std=c11 $(shell pkg-config --cflags sdl2 SDL2_image)
+LDFLAGS = $(shell pkg-config --libs sdl2 SDL2_image)
 SRC = $(wildcard src/*.c)
 OBJ = $(SRC:src/%.c=build/%.o)
 TARGET = build/renderer
 
-$(TARGET): $(OBJ)
+$(TARGET): $(OBJ) res
 	$(CC) $(OBJ) -o $(TARGET) $(LDFLAGS)
 
 build/%.o: src/%.c
 	@mkdir -p build
 	$(CC) $(CFLAGS) -c $< -o $@
 
+.PHONY: res
+res:
+	@mkdir -p build/res
+	cp -r res/* build/res/
+
 clean:
 	rm -rf build
 
 run: $(TARGET)
-	./$(TARGET)
+	cd build && ./renderer
 
 .PHONY: clean run
--- src/main.c
+++ src/main.c
@@ -1,28 +1,40 @@
 #include <SDL2/SDL_error.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_scancode.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <stdlib.h>
+#include <SDL2/SDL_image.h>
+#include <wchar.h>
 
 #define WINDOW_TITLE "sdl"
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define IMAGE_FLAGS IMG_INIT_PNG
 
 struct Game {
   SDL_Window *window;
   SDL_Renderer *renderer;
+  SDL_Texture *background;
 };
-
-void game_clean(struct Game *game);
+bool load_media(struct Game *game);
+void game_clean(struct Game *game, int exit_status);
 
 bool sdl_init(struct Game *game) {
   if (SDL_Init(SDL_INIT_EVERYTHING)) {
     fprintf(stderr, "no sdl init succ: %s\n", SDL_GetError());
     return true;
   };
+  
+  int img_init = IMG_Init(IMAGE_FLAGS);
+  if ((img_init & IMAGE_FLAGS) != IMAGE_FLAGS) { 
+    fprintf(stderr, "error img init: %s\n", SDL_GetError());
+    return true;
+  }
 
   game->window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
   if (!game->window) {
@@ -43,27 +55,71 @@ int main() {
   struct Game game = {
     .window = NULL,
     .renderer = NULL,
+    .background = NULL,
   };
   
   if (sdl_init(&game)) {
-    game_clean(&game);
+    game_clean(&game, EXIT_FAILURE);
     printf("cleaning, exiting");
-    exit(1);
   }
 
-  SDL_RenderClear(game.renderer);
-  SDL_RenderPresent(game.renderer);
-  SDL_Delay(3000);
-  game_clean(&game);
+  if (load_media(&game)) {
+    game_clean(&game, EXIT_FAILURE);
+    printf("cleaning, exiting");
+  }
+  
+  while (true) {
+    // sdl will check if user quits every frame (is this good?)
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          game_clean(&game, EXIT_SUCCESS);
+          break;
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.scancode) {
+            case SDL_SCANCODE_ESCAPE:
+              game_clean(&game, EXIT_SUCCESS);
+              break;
+            default:
+              break;
+          }
+        default:
+          break;
+      }
+    }
+
+    SDL_RenderClear(game.renderer);
+
+    SDL_RenderCopy(game.renderer, game.background, NULL, NULL);
+
+    SDL_RenderPresent(game.renderer);
+    SDL_Delay(16);
+  }
+
+  game_clean(&game, EXIT_SUCCESS);
   printf("good, exiting :)");
   return 0;
 }
 
+//loads anime
+bool load_media(struct Game *game) {
+  game->background = IMG_LoadTexture(game->renderer, "res/image.png");
+  if (!game->background) { 
+    fprintf(stderr, "error creating texture: %s\n", SDL_GetError());
+    return true;
+  }
+  return false;
+}
+
 // free the memory when exit
-void game_clean(struct Game *game) {
+void game_clean(struct Game *game, int exit_status) {
+  SDL_DestroyTexture(game->background);
   SDL_DestroyRenderer(game->renderer);
   SDL_DestroyWindow(game->window);
+  IMG_Quit();
   SDL_Quit();
+  exit(exit_status);
 }
 
 
